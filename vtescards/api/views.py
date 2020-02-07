677fd9bd2a518b1015d6748cf41623b5ca2531a3
Carlos Lucas <clucas@hotelbeds.com>
2020-02-07 12:38:50 +0100

    changed searh type to tirgram similarity

--- vtescards/api/views.py
+++ vtescards/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.exceptions import APIException
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import TrigramSimilarity
 from api.models import Card, CryptCard, LibraryCard
 from api.serializers import CardSerializer, CryptCardSerializer, LibraryCardSerializer
 
@@ -33,5 +33,7 @@ class CardSearchViewSet(ReadOnlyModelViewSet):
         if not name_param or len(name_param) < 3:
             raise APIException(code=400, detail="Parameter 'name' must be specified with a length greater than 2.")
 
-        return Card.objects.annotate(search=SearchVector('name')).filter(search=name_param)
-
+        return Card.objects \
+            .annotate(similarity=TrigramSimilarity('name', name_param)) \
+            .filter(similarity__gt=0.20) \
+            .order_by('-similarity')
