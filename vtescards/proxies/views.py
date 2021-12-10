ac76517495be50a459c5566d5f198e456316e550
Carlos Lucas <nosk14@gmail.com>
2021-12-10 15:57:35 +0100

    proxy endpoint accepts set info

--- vtescards/proxies/views.py
+++ vtescards/proxies/views.py
@@ -16,23 +16,35 @@ single_card_counter = Counter("bloodlibrary_single_card_proxies", "Amount of tim
 @require_http_methods(['POST'])
 def generate_pdf(request):
     text = request.body.decode('utf-8')
-    data = json.loads(text)
-    if not data:
+    rq_data = json.loads(text)
+    if not rq_data:
         return HttpResponseBadRequest()
 
     proxy_file = ProxyFile()
-    for card in data:
-        card_image = CardSet.objects\
-            .filter(card_id=card['id'])\
-            .exclude(image=None)\
-            .order_by('-set_id')[0].image
+    for card in rq_data:
+        if 'set' in card and card['set']:
+            result = CardSet.objects \
+                .filter(card_id=card['id'], set_id=card['set']) \
+                .exclude(image=None)
+            result = list(result)
+
+            if not result:
+                return HttpResponseBadRequest()
+
+            card_image = result[0].image
+        else:
+            card_image = CardSet.objects\
+                .filter(card_id=card['id'])\
+                .exclude(image=None)\
+                .order_by('-set_id')[0].image
+
         for _ in range(card['amount']):
             proxy_file.add_image(card_image)
 
     proxy_file.save()
 
     generated_pdfs.inc()
-    for card in data:
+    for card in rq_data:
         single_card_counter.labels(card_id=card['id']).inc(card['amount'])
 
     return FileResponse(proxy_file.serve_buffer(), as_attachment=True, filename='vtes_proxies.pdf')
