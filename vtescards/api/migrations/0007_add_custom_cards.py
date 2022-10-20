01776e141299c660fb782772c7b2332661b30aad
Carlos Lucas <nosk14@gmail.com>
2022-10-20 12:40:31 +0200

    added khazar wraith token

---
+++ vtescards/api/migrations/0007_add_custom_cards.py
@@ -0,0 +1,30 @@
+from api.models import CardSet, Set, Card
+from django.db import migrations
+
+
+def add_tokens(apps, schema_editor):
+    custom_set = Set.objects.get(id='399997')
+    LibraryCard = apps.get_model('api', 'LibraryCard')
+    lc = LibraryCard(id="199999",
+                     name="Khazar's Diary Wraith (Token)",
+                     alias="Khazar's Diary Wraith (Token)",
+                     card_type="Ally"
+                     )
+    lc.save()
+
+    CardSet(card=lc,
+            set=custom_set,
+            image="https://statics.bloodlibrary.info/img/sets/custom/KhazarWraith.png"
+            ).save()
+
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [
+        ('api', '0004_load_card_set_images'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_tokens)
+    ]
