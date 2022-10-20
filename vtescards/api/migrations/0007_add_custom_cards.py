070d1bf55d9056d8ca8c2c45e02f961f93de5b5a
Carlos Lucas <nosk14@gmail.com>
2022-10-20 12:51:57 +0200

    Fix getting card for migration

--- vtescards/api/migrations/0007_add_custom_cards.py
+++ vtescards/api/migrations/0007_add_custom_cards.py
@@ -5,14 +5,14 @@ from django.db import migrations
 def add_tokens(apps, schema_editor):
     custom_set = Set.objects.get(id='399997')
     LibraryCard = apps.get_model('api', 'LibraryCard')
-    lc = LibraryCard(id="199999",
-                     name="Khazar's Diary Wraith (Token)",
-                     alias="Khazar's Diary Wraith (Token)",
-                     card_type="Ally"
-                     )
-    lc.save()
 
-    CardSet(card=lc,
+    LibraryCard(id='199999',
+                name="Khazar's Diary Wraith (Token)",
+                alias="Khazar's Diary Wraith (Token)",
+                card_type="Ally"
+                ).save()
+
+    CardSet(card=Card.objects.get(pk='199999'),
             set=custom_set,
             image="https://statics.bloodlibrary.info/img/sets/custom/KhazarWraith.png"
             ).save()
