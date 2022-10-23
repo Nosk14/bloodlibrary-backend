80565f250571ac4c26e8dbda90dc654ff821bc61
Carlos Lucas <nosk14@gmail.com>
2022-10-23 12:47:44 +0200

    image endpoint by id

--- vtescards/api/views.py
+++ vtescards/api/views.py
@@ -48,18 +48,24 @@ class CardSearchViewSet(ReadOnlyModelViewSet):
 
 def get_card_image(request):
     name_param = request.GET.get('name', None)
-    if not name_param or len(name_param) < 2:
-        raise APIException(code=400, detail="Parameter 'name' must be specified with a length greater than 2.")
+    id_param = request.GET.get('id', None)
 
-    name_param = name_param.lower()
-    cards = Card.objects \
-        .annotate(similarity=TrigramSimilarity('alias', name_param)) \
-        .filter(similarity__gt=0.20) \
-        .order_by('-similarity')
+    if id_param:
+        card = Card.objects.get(pk=id_param)
+    else:
+        if not name_param or len(name_param) < 2:
+            raise APIException(code=400, detail="Parameter 'name' must be specified with a length greater than 2.")
 
-    if not cards:
-        raise APIException(code=404, detail="No cards found")
+        name_param = name_param.lower()
+        cards = Card.objects \
+            .annotate(similarity=TrigramSimilarity('alias', name_param)) \
+            .filter(similarity__gt=0.20) \
+            .order_by('-similarity')
 
-    img_link = cards[0].cardset_set.exclude(image='').order_by('-id')[0].image
+        if not cards:
+            raise APIException(code=404, detail="No cards found")
+        card = cards[0]
+
+    img_link = card.cardset_set.exclude(image='').order_by('-id')[0].image
 
     return redirect(img_link)
