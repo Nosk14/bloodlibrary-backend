2df0bec70dbce04b51bf2d8560c3ccad12d9f57c
Carlos Lucas <nosk14@gmail.com>
2024-02-12 14:36:20 +0100

    pod queryset fix

--- vtescards/pod/views.py
+++ vtescards/pod/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Subquery
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from api.models import Card
+from pod.models import CardShop
 from pod.serializers import CardWithShopSerializer
 
 
 class CardShopViewSet(ReadOnlyModelViewSet):
-    queryset = Card.objects.prefetch_related('shops').filter(shops__isnull=False)
+    queryset = Card.objects.filter(id__in=Subquery(CardShop.objects.values_list('card_id', flat=True)))
     serializer_class = CardWithShopSerializer
